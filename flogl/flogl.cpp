6c5a2ce0a10bfe086fd9747cf34374f4ea15a67d
Sam Nobs <3915450+samsta@users.noreply.github.com>
2021-04-20 20:41:24 +1200

    Rename particle -> LED

--- flogl/flogl.cpp
+++ flogl/flogl.cpp
@@ -31,128 +31,118 @@ const std::string FRAGMENT_SHADER =
 
 
 // CPU representation of a particle
-struct Particle{
-   Particle(double x, double y, double z): pos(glm::vec3(x,y,z)), size(0.5)
+struct LED{
+   LED(double x, double y, double z): pos(glm::vec3(x,y,z)), size(0.5)
       {
       }
 	glm::vec3 pos;
 	unsigned char r,g,b,a; // Color
 	float size;
-	float cameradistance; // *Squared* distance to the camera. if dead : -1.0f
-
-	bool operator<(const Particle& that) const {
-		// Sort in reverse order : far particles drawn first.
-		return this->cameradistance > that.cameradistance;
-	}
 };
 
 
 float vsc = 0.5;
 float hsc = 0.3;
 
-Particle ParticlesContainer[] =
+LED leds[] =
 {
-   Particle(hsc*0,  vsc*30, -20),
-   Particle(hsc*1,  vsc*29, -20),
-   Particle(hsc*2,  vsc*28, -20),
-   Particle(hsc*3,  vsc*27, -20),
-   Particle(hsc*4,  vsc*26, -20),
-   Particle(hsc*5,  vsc*25, -20),
-   Particle(hsc*6,  vsc*24, -20),
-   Particle(hsc*7,  vsc*23, -20),
-   Particle(hsc*8,  vsc*22, -20),
-   Particle(hsc*9,  vsc*21, -20),
-   Particle(hsc*10, vsc*20, -20),
-   Particle(hsc*11, vsc*19, -20),
-   Particle(hsc*12, vsc*18, -20),
-   Particle(hsc*13, vsc*17, -20),
-   Particle(hsc*14, vsc*16, -20),
-   Particle(hsc*15, vsc*15, -20),
-   Particle(hsc*16, vsc*14, -20),
-   Particle(hsc*17, vsc*13, -20),
-   Particle(hsc*18, vsc*12, -20),
-   Particle(hsc*19, vsc*11, -20),
-   Particle(hsc*20, vsc*10, -20),
-   Particle(hsc*21, vsc*9, -20),
-   Particle(hsc*22, vsc*8, -20),
-   Particle(hsc*23, vsc*7, -20),
-   Particle(hsc*24, vsc*6, -20),
-   Particle(hsc*25, vsc*5, -20),
-   Particle(hsc*26, vsc*4, -20),
-   Particle(hsc*27, vsc*3, -20),
-   Particle(hsc*28, vsc*2, -20),
-   Particle(hsc*29, vsc*1, -20),
-   Particle(hsc*30, vsc*0, -20),
+   LED(hsc*0,  vsc*30, -20),
+   LED(hsc*1,  vsc*29, -20),
+   LED(hsc*2,  vsc*28, -20),
+   LED(hsc*3,  vsc*27, -20),
+   LED(hsc*4,  vsc*26, -20),
+   LED(hsc*5,  vsc*25, -20),
+   LED(hsc*6,  vsc*24, -20),
+   LED(hsc*7,  vsc*23, -20),
+   LED(hsc*8,  vsc*22, -20),
+   LED(hsc*9,  vsc*21, -20),
+   LED(hsc*10, vsc*20, -20),
+   LED(hsc*11, vsc*19, -20),
+   LED(hsc*12, vsc*18, -20),
+   LED(hsc*13, vsc*17, -20),
+   LED(hsc*14, vsc*16, -20),
+   LED(hsc*15, vsc*15, -20),
+   LED(hsc*16, vsc*14, -20),
+   LED(hsc*17, vsc*13, -20),
+   LED(hsc*18, vsc*12, -20),
+   LED(hsc*19, vsc*11, -20),
+   LED(hsc*20, vsc*10, -20),
+   LED(hsc*21, vsc*9, -20),
+   LED(hsc*22, vsc*8, -20),
+   LED(hsc*23, vsc*7, -20),
+   LED(hsc*24, vsc*6, -20),
+   LED(hsc*25, vsc*5, -20),
+   LED(hsc*26, vsc*4, -20),
+   LED(hsc*27, vsc*3, -20),
+   LED(hsc*28, vsc*2, -20),
+   LED(hsc*29, vsc*1, -20),
+   LED(hsc*30, vsc*0, -20),
 
-   Particle(hsc*28,  0, -20),
-   Particle(hsc*26,  0, -20),
-   Particle(hsc*24,  0, -20),
-   Particle(hsc*22,  0, -20),
-   Particle(hsc*20,  0, -20),
-   Particle(hsc*18,  0, -20),
-   Particle(hsc*16,  0, -20),
-   Particle(hsc*14,  0, -20),
-   Particle(hsc*12,  0, -20),
-   Particle(hsc*10,  0, -20),
-   Particle(hsc*8,   0, -20),
-   Particle(hsc*6,   0, -20),
-   Particle(hsc*4,   0, -20),
-   Particle(hsc*2,   0, -20),
-   Particle(hsc*0,   0, -20),
-   Particle(-hsc*2,   0, -20),
-   Particle(-hsc*4,   0, -20),
-   Particle(-hsc*6,   0, -20),
-   Particle(-hsc*8,   0, -20),
-   Particle(-hsc*10,  0, -20),
-   Particle(-hsc*12,  0, -20),
-   Particle(-hsc*14,  0, -20),
-   Particle(-hsc*16,  0, -20),
-   Particle(-hsc*18,  0, -20),
-   Particle(-hsc*20,  0, -20),
-   Particle(-hsc*22,  0, -20),
-   Particle(-hsc*24,  0, -20),
-   Particle(-hsc*26,  0, -20),
-   Particle(-hsc*28,  0, -20),
+   LED(hsc*28,  0, -20),
+   LED(hsc*26,  0, -20),
+   LED(hsc*24,  0, -20),
+   LED(hsc*22,  0, -20),
+   LED(hsc*20,  0, -20),
+   LED(hsc*18,  0, -20),
+   LED(hsc*16,  0, -20),
+   LED(hsc*14,  0, -20),
+   LED(hsc*12,  0, -20),
+   LED(hsc*10,  0, -20),
+   LED(hsc*8,   0, -20),
+   LED(hsc*6,   0, -20),
+   LED(hsc*4,   0, -20),
+   LED(hsc*2,   0, -20),
+   LED(hsc*0,   0, -20),
+   LED(-hsc*2,   0, -20),
+   LED(-hsc*4,   0, -20),
+   LED(-hsc*6,   0, -20),
+   LED(-hsc*8,   0, -20),
+   LED(-hsc*10,  0, -20),
+   LED(-hsc*12,  0, -20),
+   LED(-hsc*14,  0, -20),
+   LED(-hsc*16,  0, -20),
+   LED(-hsc*18,  0, -20),
+   LED(-hsc*20,  0, -20),
+   LED(-hsc*22,  0, -20),
+   LED(-hsc*24,  0, -20),
+   LED(-hsc*26,  0, -20),
+   LED(-hsc*28,  0, -20),
 
    
-   Particle(-hsc*30, vsc*0, -20),
-   Particle(-hsc*29, vsc*1, -20),
-   Particle(-hsc*28, vsc*2, -20),
-   Particle(-hsc*27, vsc*3, -20),
-   Particle(-hsc*26, vsc*4, -20),
-   Particle(-hsc*25, vsc*5, -20),
-   Particle(-hsc*24, vsc*6, -20),
-   Particle(-hsc*23, vsc*7, -20),
-   Particle(-hsc*22, vsc*8, -20),
-   Particle(-hsc*21, vsc*9, -20),
-   Particle(-hsc*20, vsc*10, -20),
-   Particle(-hsc*19, vsc*11, -20),
-   Particle(-hsc*18, vsc*12, -20),
-   Particle(-hsc*17, vsc*13, -20),
-   Particle(-hsc*16, vsc*14, -20),
-   Particle(-hsc*15, vsc*15, -20),
-   Particle(-hsc*14, vsc*16, -20),
-   Particle(-hsc*13, vsc*17, -20),
-   Particle(-hsc*12, vsc*18, -20),
-   Particle(-hsc*11, vsc*19, -20),
-   Particle(-hsc*10, vsc*20, -20),
-   Particle(-hsc*9,  vsc*21, -20),
-   Particle(-hsc*8,  vsc*22, -20),
-   Particle(-hsc*7,  vsc*23, -20),
-   Particle(-hsc*6,  vsc*24, -20),
-   Particle(-hsc*5,  vsc*25, -20),
-   Particle(-hsc*4,  vsc*26, -20),
-   Particle(-hsc*3,  vsc*27, -20),
-   Particle(-hsc*2,  vsc*28, -20),
-   Particle(-hsc*1,  vsc*29, -20),
-   Particle(-hsc*0,  vsc*30, -20),
+   LED(-hsc*30, vsc*0, -20),
+   LED(-hsc*29, vsc*1, -20),
+   LED(-hsc*28, vsc*2, -20),
+   LED(-hsc*27, vsc*3, -20),
+   LED(-hsc*26, vsc*4, -20),
+   LED(-hsc*25, vsc*5, -20),
+   LED(-hsc*24, vsc*6, -20),
+   LED(-hsc*23, vsc*7, -20),
+   LED(-hsc*22, vsc*8, -20),
+   LED(-hsc*21, vsc*9, -20),
+   LED(-hsc*20, vsc*10, -20),
+   LED(-hsc*19, vsc*11, -20),
+   LED(-hsc*18, vsc*12, -20),
+   LED(-hsc*17, vsc*13, -20),
+   LED(-hsc*16, vsc*14, -20),
+   LED(-hsc*15, vsc*15, -20),
+   LED(-hsc*14, vsc*16, -20),
+   LED(-hsc*13, vsc*17, -20),
+   LED(-hsc*12, vsc*18, -20),
+   LED(-hsc*11, vsc*19, -20),
+   LED(-hsc*10, vsc*20, -20),
+   LED(-hsc*9,  vsc*21, -20),
+   LED(-hsc*8,  vsc*22, -20),
+   LED(-hsc*7,  vsc*23, -20),
+   LED(-hsc*6,  vsc*24, -20),
+   LED(-hsc*5,  vsc*25, -20),
+   LED(-hsc*4,  vsc*26, -20),
+   LED(-hsc*3,  vsc*27, -20),
+   LED(-hsc*2,  vsc*28, -20),
+   LED(-hsc*1,  vsc*29, -20),
+   LED(-hsc*0,  vsc*30, -20),
 };
 
-int MaxParticles = sizeof(ParticlesContainer)/sizeof(*ParticlesContainer);
-
-void SortParticles(){
-	std::sort(&ParticlesContainer[0], &ParticlesContainer[MaxParticles]);
-}
+int MaxLEDs = sizeof(leds)/sizeof(*leds);
 
 int main( void )
 {
@@ -224,13 +214,13 @@ int main( void )
 	GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
 
 	
-	static GLfloat* g_particule_position_size_data = new GLfloat[MaxParticles * 4];
-	static GLubyte* g_particule_color_data         = new GLubyte[MaxParticles * 4];
+	static GLfloat* g_led_position_size_data = new GLfloat[MaxLEDs * 4];
+	static GLubyte* g_led_color_data         = new GLubyte[MaxLEDs * 4];
 
 	GLuint Texture = loadDDS(blobs::LedTexture, sizeof(blobs::LedTexture));
 
-	// The VBO containing the 4 vertices of the particles.
-	// Thanks to instancing, they will be shared by all particles.
+	// The VBO containing the 4 vertices of the leds.
+	// Thanks to instancing, they will be shared by all leds.
 	static const GLfloat g_vertex_buffer_data[] = { 
 		 -0.5f, -0.5f, 0.0f,
 		  0.5f, -0.5f, 0.0f,
@@ -242,19 +232,19 @@ int main( void )
 	glBindBuffer(GL_ARRAY_BUFFER, billboard_vertex_buffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	// The VBO containing the positions and sizes of the particles
-	GLuint particles_position_buffer;
-	glGenBuffers(1, &particles_position_buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
+	// The VBO containing the positions and sizes of the leds
+	GLuint leds_position_buffer;
+	glGenBuffers(1, &leds_position_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, leds_position_buffer);
 	// Initialize with empty (NULL) buffer : it will be updated later, each frame.
-	glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, MaxLEDs * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW);
 
-	// The VBO containing the colors of the particles
-	GLuint particles_color_buffer;
-	glGenBuffers(1, &particles_color_buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, particles_color_buffer);
+	// The VBO containing the colors of the leds
+	GLuint leds_color_buffer;
+	glGenBuffers(1, &leds_color_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, leds_color_buffer);
 	// Initialize with empty (NULL) buffer : it will be updated later, each frame.
-	glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, MaxLEDs * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW);
 
 	
 	double lastTime = glfwGetTime();
@@ -273,7 +263,7 @@ int main( void )
 		glm::mat4 ViewMatrix = getViewMatrix();
       
 
-		// We will need the camera's position in order to sort the particles
+		// We will need the camera's position in order to sort the leds
 		// w.r.t the camera's distance.
 		// There should be a getCameraPosition() function in common/controls.cpp, 
 		// but this works too.
@@ -282,39 +272,28 @@ int main( void )
 		glm::mat4 ViewProjectionMatrix = ProjectionMatrix * ViewMatrix;
  
 
-		for(int i=0; i<MaxParticles; i++){
+		for(int i=0; i<MaxLEDs; i++){
 
-			Particle& p = ParticlesContainer[i]; // shortcut
+			LED& p = leds[i]; // shortcut
 
          p.r = (sin(5*lastTime+float(i)/(10))+1)*127;
          p.g = (cos(5*lastTime+float(i)/(10))+1)*127;
          p.b = (sin(10*lastTime+float(i)/(10))+1)*127;
          p.a = 255;
          
-         // Simulate simple physics : gravity only, no collisions
-         //p.speed += glm::vec3(0.0f,-9.81f, 0.0f) * (float)delta * 0.5f;
-         // p.pos += p.speed * (float)delta;
-         p.cameradistance = glm::length2( p.pos - CameraPosition );
-         //ParticlesContainer[i].pos += glm::vec3(0.0f,10.0f, 0.0f) * (float)delta;
-
          // Fill the GPU buffer
-         g_particule_position_size_data[4*i+0] = p.pos.x;
-         g_particule_position_size_data[4*i+1] = p.pos.y;
-         g_particule_position_size_data[4*i+2] = p.pos.z;
+         g_led_position_size_data[4*i+0] = p.pos.x;
+         g_led_position_size_data[4*i+1] = p.pos.y;
+         g_led_position_size_data[4*i+2] = p.pos.z;
 			
-         g_particule_position_size_data[4*i+3] = p.size;
+         g_led_position_size_data[4*i+3] = p.size;
 			
-         g_particule_color_data[4*i+0] = p.r;
-         g_particule_color_data[4*i+1] = p.g;
-         g_particule_color_data[4*i+2] = p.b;
-         g_particule_color_data[4*i+3] = p.a;
+         g_led_color_data[4*i+0] = p.r;
+         g_led_color_data[4*i+1] = p.g;
+         g_led_color_data[4*i+2] = p.b;
+         g_led_color_data[4*i+3] = p.a;
          
       }
-
-		//SortParticles();
-      //printf("%f, %f, %f\n", CameraPosition.x, CameraPosition.y, CameraPosition.z);
-
-
 
 		// Update the buffers that OpenGL uses for rendering.
 		// There are much more sophisticated means to stream data from the CPU to the GPU, 
@@ -322,13 +301,13 @@ int main( void )
 		// http://www.opengl.org/wiki/Buffer_Object_Streaming
 
 
-		glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
-		glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
-		glBufferSubData(GL_ARRAY_BUFFER, 0, MaxParticles * sizeof(GLfloat) * 4, g_particule_position_size_data);
+		glBindBuffer(GL_ARRAY_BUFFER, leds_position_buffer);
+		glBufferData(GL_ARRAY_BUFFER, MaxLEDs * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
+		glBufferSubData(GL_ARRAY_BUFFER, 0, MaxLEDs * sizeof(GLfloat) * 4, g_led_position_size_data);
 
-		glBindBuffer(GL_ARRAY_BUFFER, particles_color_buffer);
-		glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
-		glBufferSubData(GL_ARRAY_BUFFER, 0, MaxParticles * sizeof(GLubyte) * 4, g_particule_color_data);
+		glBindBuffer(GL_ARRAY_BUFFER, leds_color_buffer);
+		glBufferData(GL_ARRAY_BUFFER, MaxLEDs * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
+		glBufferSubData(GL_ARRAY_BUFFER, 0, MaxLEDs * sizeof(GLubyte) * 4, g_led_color_data);
 
 
 		glEnable(GL_BLEND);
@@ -361,9 +340,9 @@ int main( void )
 			(void*)0            // array buffer offset
 		);
 		
-		// 2nd attribute buffer : positions of particles' centers
+		// 2nd attribute buffer : positions of leds' centers
 		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
+		glBindBuffer(GL_ARRAY_BUFFER, leds_position_buffer);
 		glVertexAttribPointer(
 			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
 			4,                                // size : x + y + z + size => 4
@@ -373,9 +352,9 @@ int main( void )
 			(void*)0                          // array buffer offset
 		);
 
-		// 3rd attribute buffer : particles' colors
+		// 3rd attribute buffer : leds' colors
 		glEnableVertexAttribArray(2);
-		glBindBuffer(GL_ARRAY_BUFFER, particles_color_buffer);
+		glBindBuffer(GL_ARRAY_BUFFER, leds_color_buffer);
 		glVertexAttribPointer(
 			2,                                // attribute. No particular reason for 1, but must match the layout in the shader.
 			4,                                // size : r + g + b + a => 4
@@ -389,16 +368,16 @@ int main( void )
 		// The first parameter is the attribute buffer we're talking about.
 		// The second parameter is the "rate at which generic vertex attributes advance when rendering multiple instances"
 		// http://www.opengl.org/sdk/docs/man/xhtml/glVertexAttribDivisor.xml
-		glVertexAttribDivisor(0, 0); // particles vertices : always reuse the same 4 vertices -> 0
+		glVertexAttribDivisor(0, 0); // leds vertices : always reuse the same 4 vertices -> 0
 		glVertexAttribDivisor(1, 1); // positions : one per quad (its center)                 -> 1
 		glVertexAttribDivisor(2, 1); // color : one per quad                                  -> 1
 
-		// Draw the particules !
+		// Draw the leds !
 		// This draws many times a small triangle_strip (which looks like a quad).
 		// This is equivalent to :
-		// for(i in ParticlesCount) : glDrawArrays(GL_TRIANGLE_STRIP, 0, 4), 
+		// for(i in LEDsCount) : glDrawArrays(GL_TRIANGLE_STRIP, 0, 4), 
 		// but faster.
-		glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, MaxParticles);
+		glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, MaxLEDs);
 
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
@@ -413,11 +392,11 @@ int main( void )
 		   glfwWindowShouldClose(window) == 0 );
 
 
-	delete[] g_particule_position_size_data;
+	delete[] g_led_position_size_data;
 
 	// Cleanup VBO and shader
-	glDeleteBuffers(1, &particles_color_buffer);
-	glDeleteBuffers(1, &particles_position_buffer);
+	glDeleteBuffers(1, &leds_color_buffer);
+	glDeleteBuffers(1, &leds_position_buffer);
 	glDeleteBuffers(1, &billboard_vertex_buffer);
 	glDeleteProgram(programID);
 	glDeleteTextures(1, &Texture);
